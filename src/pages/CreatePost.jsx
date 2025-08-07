0a8e2bea58f394e64be8d4f36cb32afe00e87e0c
priyanshsingh <singh.priyansh1391@gmail.com>
2025-08-07 17:53:25 +0530

    day 5

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { dummyUserData } from '../assets/assets'
 
 const CreatePost = () => {
+
+  const [content, setContent] = useState('')
+  const [images, setImages] = useState([])
+
+  const [loading, setLoading] = useState(false)
+
+  const user = dummyUserData;
+
+
   return (
-    <div>
-      
+    <div className='min-h-screen bg-gradient-to-r from-slate-50 to-white'>
+      <div className='max-w-6xl mx-auto p-6'>
+      {/*Title */}
+      <div className='mb-8'>
+      <h1 className='text-3xl font-bold text-slate-900 mb-2'>Create Post</h1>
+      <p className='text-slate-600'>Share your thoughts with the world</p>
+      </div>
+
+      {/*form */}
+      <div className='max-w-xl bg-white p-4 sm:pb-3 rounded-xl shadow-md space-y-4'>
+      {/**header */}
+      <div>
+        <img src={user.profile_picture} alt="" className='w-12 h-12
+        rounded-full shadow' />
+        <div>
+          <h2 className='font-semibold'>{user.full_name}</h2>
+          <p>@{user.username}</p>
+        </div>
+      </div>
+      </div>
+    </div>
     </div>
   )
 }
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { dummyConnectionsData } from '../assets/assets';
+import { Search } from 'lucide-react';
+import UserCard from '../Components/UserCard';
 
 const Discover = () => {
 
@@ -22,9 +24,27 @@ const Discover = () => {
       <div className='max-w-6xl mx-auto p-6'>
       {/*title */}
        <div className='mb-8'>
-          <h1 className='text-3xl font-bold text-slate-900 mb-2'>Connections</h1>
-          <p className='text-slate-600'>Manage your network and discover new connections</p>
+          <h1 className='text-3xl font-bold text-slate-900 mb-2'>Discover People</h1>
+          <p className='text-slate-600'>Conect with amazing people and grow your network</p>
         </div>
+        {/*sEARCH */}
+        <div className='mb-8 shadow-md rounded-md border border-slate-200/60 bg-white/80'>
+        <div className='p-6'>
+          <div className='relative'>
+            <Search className = 'absolute left-3 top-1/2 transform -translate-y-1/2 text-slate-400 w-5 h-5' />
+            <input type='text' placeholder='Search people by name, username,bio,or location...' className='pl-10 sm:pl-12 py-2 w-full border border-gray-300 rounded-md max-sm:text-sm ' onChange={(e)=>setInput(e.target.value)} value={input} onKeyUp={handleSearch}/>
+
+          </div>
+        </div>
+        </div>
+        <div className='flex flex-wrap gap-6 '>
+          {users.map((users)=>(
+            <UserCard user ={users} key={users._id}/>
+          ))}
+        </div>
+        {
+          loading && (<Loading height='60vh'/>)
+        }
       </div>
       
     </div>
