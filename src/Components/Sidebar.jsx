54d7680887bd07479dc2c9c21640efd5671c8102
priyanshsingh <singh.priyansh1391@gmail.com>
2025-08-12 00:03:50 +0530

    changed

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -8,7 +8,7 @@ import{UserButton, useClerk} from '@clerk/clerk-react'
 const Sidebar = ({sidebarOpen, setSidebarOpen}) => {
   const navigate = useNavigate();
   const user = dummyUserData
-  const {signout} = useClerk()
+  const {signOut} = useClerk()
   return (
     <div className={`w-60 xl:w-72 bg-white border-r border-gray-200 flex flex-col justify-between items-center max-sm:absolute top-0 bottom-0 z-20 ${sidebarOpen ? 'translate-x-0' : 'max-sm:-translate-x-full'} transition-all duration-300 ease-in-out`}>
      <div className='w-full'>
@@ -30,7 +30,7 @@ const Sidebar = ({sidebarOpen, setSidebarOpen}) => {
       <p className='text-xs text-gray-500'>@{user.username}</p>
     </div>
 </div>
-<LogOut onClick={signout} className='w-4.5 text-gray-400 hover:text-gray-700 transition cursor-pointer'/>
+<LogOut onClick={signOut} className='w-4.5 text-gray-400 hover:text-gray-700 transition cursor-pointer'/>
      </div>
     </div>
   )
